ea633b30024a778c06e7ab2a009ab734e62ce6ff
Nefta <neftarguez@gmail.com>
2024-10-31 09:24:50 +0000

    Cambios en el routing

--- PassGenerator Front/passgenerator/src/app/app-routing.module.ts
+++ PassGenerator Front/passgenerator/src/app/app-routing.module.ts
@@ -12,7 +12,7 @@ import { MyProfileComponent } from './components/my-profile/my-profile.component
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: '**', redirectTo: 'home' },
+  //{ path: '**', redirectTo: 'home' },
   { path: 'home', component: HomeComponent},
   { path: 'passwords', component: PasswordsComponent, canActivate: [authGuard]},
   { path: 'profile', component: MyProfileComponent, canActivate: [authGuard]},
